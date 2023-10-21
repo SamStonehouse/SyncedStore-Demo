360f842bedc75d362e4371241ecd8fdf1cf8f9e3
Sam Stonehouse <sgstonehouse@googlemail.com>
2023-10-21 10:08:07 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# SyncedStore Demo Project
+
+A test project for [SyncedStore](https://syncedstore.org/docs/basics/example)
---
+++ src/app.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import styles from './app.module.css';
+
+import './app.css';
+import './variables.css';
+
+const App = (): JSX.Element => {
+  return (
+    <div className={styles.app}>
+      App
+    </div>
+  );
+};
+
+export default App;
